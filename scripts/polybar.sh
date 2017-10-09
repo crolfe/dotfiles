45303099d7a4373932998fb5fcd97e9039cf5f38
Colin Rolfe <crolfe@gmail.com>
2017-10-09 11:27:54 -0700

    streamline polybar

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -6,11 +6,3 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 MONITOR=eDP1 polybar -c $HOME/.polybar.conf top &
 MONITOR=HDMI2 polybar -c $HOME/.polybar.conf top &
-MONITOR=eDP1 polybar -c $HOME/.polybar.conf bottom &
-MONITOR=HDMI2 polybar -c $HOME/.polybar.conf bottom &
-
-#if [ $(hostname) = 'annedouwe-pc' ]; then
-	#MONITOR=VGA-1 polybar -c $HOME/.polybar.conf bar &
-	#MONITOR=DVI-0 polybar -c $HOME/.polybar.conf bar &
-#else
-#fi
